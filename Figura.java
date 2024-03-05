c85368c9200b6944d1b7b93847597dd40f263d24
Agustin Canterla <acangon650@g.educaand.es>
2024-03-05 19:27:01 +0100

    Commit Cambios

--- Figura.java
+++ Figura.java
@@ -1,5 +1,23 @@
+/**
+ *Representa la estructura básica de figuras geométricas.
+ *Las subclases deben implementar los métodos para calcular el área y el perímetro.
+ *
+ *@author Agustin_Canterla
+ */
 public abstract class Figura {
+
+    /**
+     *Retorna el área de la figura.
+     *
+     *@return Área como double
+     */	
     public abstract double area();
+    
+    /**
+     *Retorna el perímetro de la figura.
+     *
+     *@return Perímetro como double
+     */
     public abstract double perimetro();
 }
 
